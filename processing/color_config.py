c6f06edb63f5dc1f10b41e2d65ba788267c8ac03
daniilmiagkov <d.miagkov@gmail.com>
2025-05-25 23:31:35 +0300

    update color_config

--- processing/color_config.py
+++ processing/color_config.py
@@ -4,22 +4,21 @@ from typing import Tuple
 @dataclass
 class ColorConfig:
     # --- Новое поле для медианной фильтрации ---
-    median_blur_size: int = 3          # Нечётный размер ядра для cv2.medianBlur :contentReference[oaicite:1]{index=1}
-
+    median_blur_size: int = 3       
     adaptive_thresh: bool = True
-    binary_thresh: int = 10
+    binary_thresh: int = 100
 
     morph_kernel_size: int = 2
-    morph_iterations: int = 1
+    morph_iterations: int = 3
 
-    distance_transform_mask: int = 3
-    foreground_threshold_ratio: float = 0.04
-    dilate_iterations: int = 2
+    distance_transform_mask: int = 1
+    foreground_threshold_ratio: float = 0.5
+    dilate_iterations: int = 3
 
-    min_particle_size: float = 4
-    max_particle_size: float = 65
+    min_particle_size: float = 3
+    max_particle_size: float = 100
 
     bbox_color: Tuple[int, int, int] = (0, 255, 0)
-    bbox_thickness: int = 2
+    bbox_thickness: int = 1
     font_scale: float = 0.5
-    font_thickness: int = 1
+    font_thickness: int = 0
